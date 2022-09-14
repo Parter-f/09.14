7ec6834437772a8d00013ff82a8caee130176bfa
Árpás Péter <arpaspeter0@gmail.com>
2022-09-14 12:51:38 +0200

    Src megkreálva

--- 0914.js
+++
@@ -1,4 +0,0 @@
-import {veletlen} from './matek.js';
-import {NEV , LOREM} from './kulonleges.js';
-console.log(veletlen(5,1000));
-console.log(NEV + ' ' + LOREM);
---
+++ src/0914.js
@@ -0,0 +1,17 @@
+import {veletlen} from './matek.js';
+import {NEV , LOREM} from './kulonleges.js';
+//import { concat } from 'lodash';
+import _ from 'lodash';
+
+
+console.log(veletlen(5,1000));
+console.log(NEV + ' ' + LOREM);
+
+let t1 = [1 , 455, 6];
+let t2 = [5 ,155 ,2];
+let t3 = [10];
+
+let osszefuzott = _.concat(t1 , t2 , t3);
+console.log(osszefuzott);
+
+
